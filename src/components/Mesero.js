0d22053e6deb45c4424cdd358abb119a29f2e5bd
Nathaly Taucare <nathaly.tau@gmail.com>
2021-06-27 21:38:35 -0400

    componentes primera y segunda pagina

---
+++ src/components/Mesero.js
@@ -0,0 +1,37 @@
+const Mesero = () => {
+  return (
+    <div class="mesero-container">
+      <div class="mesa1">
+        <h2>MESA 1</h2>
+        <input type="text"></input>
+        <input type="image" src="https://i.ibb.co/T8bVg6p/visto.png" alt="visto"></input>
+      </div>
+      <div class="mesa2">
+        <h2>MESA 2</h2>
+        <input type="text"></input>
+        <input type="image" src="https://i.ibb.co/T8bVg6p/visto.png" alt="visto"></input>
+      </div>
+      <div class="mesa3">
+        <h2>MESA 3</h2>
+        <input type="text"></input>
+        <input type="image" src="https://i.ibb.co/T8bVg6p/visto.png" alt="visto"></input>
+      </div>
+      <div class="mesa4">
+        <h2>MESA 4</h2>
+        <input type="text"></input>
+        <input type="image" src="https://i.ibb.co/T8bVg6p/visto.png" alt="visto"></input>
+      </div>
+      <div class="mesa5">
+        <h2>MESA 5</h2>
+        <input type="text"></input>
+        <input type="image" src="https://i.ibb.co/T8bVg6p/visto.png" alt="visto"></input>
+      </div>
+      <div class="mesa6">
+        <h2>MESA 6</h2>
+        <input type="text"></input>
+        <input type="image" src="https://i.ibb.co/T8bVg6p/visto.png" alt="visto"></input>
+      </div>
+    </div>
+  )
+}
+export default Mesero;
