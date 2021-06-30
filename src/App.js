1ad9544ab77fc623c9b1c5a2ceef5808dc6b49bb
Gisela Donoso <giselapazvivian@gmail.com>
2021-06-30 10:29:47 -0400

    Components: Home, waiter and menu

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 //import logo from './logo.svg';
 import './App.css';
-<<<<<<< HEAD
-import Home from './components/Home'
-import Waiter from './components/Waiter'
-// import Logo from './components/Logo'
-import React from "react";
-=======
 import React,{useState} from "react";
 import Home from './components/Home'
 import Waiter from './components/Waiter'
 import Menu from './components/Menu'
 import nameContext from './nameContext';
 
->>>>>>> c49a859608c5caf89d43d187094bdcde9e896d45
 import {
   BrowserRouter as Router,
   Switch,
@@ -28,34 +21,12 @@ function App() {
   <nameContext.Provider value={{name,updateName}}>
     <Router>
       <div className="header">
-<<<<<<< HEAD
-        <nav>
-          <ul>
-            <li className='linkHome'>
-              <Link to="/">Inicio</Link>
-            </li>
-            <li className='linkWainter'>
-              <Link to="/Waiter">Mesero</Link>
-            </li>
-            <li className='linkUsers'>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/Waiter">
-            <Waiter />
-=======
         <Switch>
         <Route path="/Waiter" exact>
             <Waiter />
           </Route>
           <Route path="/Menu">
             <Menu />
->>>>>>> c49a859608c5caf89d43d187094bdcde9e896d45
           </Route>
           <Route path="/">
             <Home />
