c49a859608c5caf89d43d187094bdcde9e896d45
Nathaly Taucare <nathaly.tau@gmail.com>
2021-06-30 09:15:34 -0400

    Componentes: Home,Waiter y Menu

---
+++ src/components/Home.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+import logoSushibar from "../images/logoSushibar.png";
+import cocina from "../images/cocina.png";
+import meserx from "../images/meserx.png";
+
+const Home = () => {
+  const history = useHistory();
+  return (
+    <div className="homeContainer">
+      <img src={logoSushibar} alt="Sushibar" className="logoSushibar" />
+      <input
+        type="image"
+        src={cocina}
+        alt="Botón cocina"
+        className="cocinaImg"
+      />
+      <input
+        type="image"
+        onClick={() => history.push("/Waiter")}
+        src={meserx}
+        alt="Botón mesero"
+        className="meserxImg"
+      />
+    </div>
+  );
+};
+export default Home;
---
+++ src/components/Waiter.jsx
@@ -0,0 +1,53 @@
+import React, {useContext}from "react";
+import check from "../images/check.png";
+import arrow from "../images/arrow.png";
+import { useHistory } from 'react-router-dom';
+import nameContext from "../nameContext";
+
+const Waiter = () => {
+  const history = useHistory();
+  const {name , updateName } = useContext(nameContext)
+  return (
+    <div className="waiterContainer">
+      <div className="table1">
+        <h2>MESA 1</h2>
+        <input type="text" value={name} onChange={(e)=> {updateName(e.target.value)}}></input>
+        <input 
+        type="image" 
+        onClick={() => history.push("/Menu")}
+        src={check} 
+        alt="check">
+        </input>
+      </div>
+      <div className="table2">
+        <h2>MESA 2</h2>
+        <input type="text"></input>
+        <input type="image" src={check} alt="check"></input>
+      </div>
+      <div className="table3">
+        <h2>MESA 3</h2>
+        <input type="text"></input>
+        <input type="image" src={check} alt="check"></input>
+      </div>
+      <div className="table4">
+        <h2>MESA 4</h2>
+        <input type="text"></input>
+        <input type="image" src={check} alt="check"></input>
+      </div>
+      <div className="table5">
+        <h2>MESA 5</h2>
+        <input type="text"></input>
+        <input type="image" src={check} alt="check"></input>
+      </div>
+      <div className="table6">
+        <h2>MESA 6</h2>
+        <input type="text"></input>
+        <input type="image" src={check} alt="check"></input>
+      </div>
+      <div>
+        <input type="image" onClick={() => history.goBack()} src={arrow} alt="arrow"></input>
+      </div>
+    </div>
+  );
+};
+export default Waiter;
